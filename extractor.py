59386d9042f73a8186c21aaa8836020f1c4f0b75
yaniv singerman <yaniv.singerman@gmail.com>
2022-08-03 14:55:58 +0300

    try extractor

--- extractor.py
+++ extractor.py
@@ -4,32 +4,39 @@ from operator import itemgetter
 import pandas as pd
 from pathlib import Path
 import os
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 
+from config import master_directory_path_for_df_save
 from config import remote_pem_path
 from config import user
 
 from jstat_capture import node_ips
-from jstat_capture import cluster_id
-from jstat_capture import cluster_label
-from jstat_capture import nested_instance_ip
 from jstat_capture import jps_command
 
-
-
-PEM = remote_pem_path
 USERNAME = user
+PEM = remote_pem_path
+
 pids = jps_command()
 
-
 ips = node_ips()
+CHANGE_PATH = master_directory_path_for_df_save
 
 
-CHANGE_PATH = r'C:\\Users\yaniv\Documents\get-a-job\projects\emr_jstat\jstat_outputs'
-date = date.today()
-last_hour = datetime.now() - timedelta(hours=1)
+def ec2_worker_label(ips=ips):
+    s = []
+    for ip in ips:
+        throwaway = []
+        for ch in ip:
+            if ch.isdigit():
+                throwaway.append(ch)
+        s.append(throwaway)
+    ec2_label = []
+    for i, _ in enumerate(s):
+        ec2_label.append(''.join(s[i][-5:-1]))
+    return ec2_label
 
-def extract_files(ips=ips, username=USERNAME, key_file=PEM, PIDs=pids ):
+
+def extract_files(ips=ips, username=USERNAME, key_file=PEM, pids=pids, ec2_label=ec2_worker_label()):
     """
     the function opens the requested files, reads it, and saves O,FGC, and FGCT
      to a local folder as a csv. the files are saved in the following path
@@ -37,7 +44,7 @@ def extract_files(ips=ips, username=USERNAME, key_file=PEM, PIDs=pids ):
     :return:
     """
 
-    last_four_instance_chars = nested_instance_label()
+    # last_four_instance_chars = nested_instance_label()
 
 
     k = paramiko.RSAKey.from_private_key_file(f'{key_file}')
@@ -53,7 +60,7 @@ def extract_files(ips=ips, username=USERNAME, key_file=PEM, PIDs=pids ):
             allow_agent=False,
             look_for_keys=False
             )
-        for pid in PIDs[i]:
+        for pid in pids[i]:
             print(ip, pid)
             sftp = ssh.open_sftp()
             readfile = sftp.open(filename=f'/tmp/jstat_output/jstat_{pid}', mode='r', bufsize=32768)
@@ -68,15 +75,19 @@ def extract_files(ips=ips, username=USERNAME, key_file=PEM, PIDs=pids ):
                 dtime = pd.to_datetime(d1)
                 jstat.insert(0, "DateTime", dtime, allow_duplicates=True)
 
-                os.chdir(CHANGE_PATH)
-                output_dir = Path(f"cluster_{last_four_cluster_chars[cl]}\\instance_{last_four_instance_chars[cl][ip]}\\")
+                if not os.path.exists(CHANGE_PATH):
+                    os.mkdir(CHANGE_PATH)
+                    os.chdir(CHANGE_PATH)
+                else:
+                    os.chdir(CHANGE_PATH)
+                output_dir = Path(f"instance_{ec2_label[i]}")
                 output_dir.mkdir(parents=True, exist_ok=True)
-                final_dest = f"cluster_{last_four_cluster_chars[cl]}\instance_{last_four_instance_chars[cl][ip]}\jstat_{pid}.csv"
+                final_dest = f"instance_{ec2_label[i]}/jstat_{pid}.csv"
                 jstat.to_csv(final_dest, mode='a', index=False, header=False)
             sftp.truncate(path=f'/tmp/jstat_output/jstat_{pid}', size=0)
 
 
 
-# if '__name__' == '__main__':
-extract_files()
+if '__name__' == '__main__':
+    extract_files()
 
