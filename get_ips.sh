6a4cda67a0deb1707939be8979e57b7fa68be13f
yaniv singerman <yaniv.singerman@gmail.com>
2022-08-14 18:22:50 +0300

    finalized get_ips.sh

--- get_ips.sh
+++ get_ips.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-user=$(awk -F "=" '/User/ {print $2}' config.ini)
-FILE=/home/$user/emr_jstat_project/worker_ips.txt
-yarn node -list 2> /dev/null | grep internal | cut -d' ' -f1 | cut -d: -f1 >> $FILE
+user=$(awk -F "=" '/^User/ {print $2}' config.ini)
+File=(/home/$user/emr_jstat_project/worker_ips.txt)
+yarn node -list 2> /dev/null | grep internal | cut -d' ' -f1 | cut -d: -f1 >> $File
