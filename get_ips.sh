30aa20feca5110af26839cd6b3b97fd786ff888b
yaniv singerman <yaniv.singerman@gmail.com>
2022-08-14 16:50:56 +0300

    checking

--- get_ips.sh
+++ get_ips.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source <(grep=config.ini)
 User=$User
+echo User
+
 FILE=/home/!User/emr_jstat_project/worker_ips.txt
 yarn node -list 2> /dev/null | grep internal | cut -d' ' -f1 | cut -d: -f1 >> $FILE
