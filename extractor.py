8afa9cee57688eb04127d56da30f2aa1f0a9f8a4
yaniv singerman <yaniv.singerman@gmail.com>
2022-08-14 17:27:03 +0300

    try new

--- extractor.py
+++ extractor.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import os
 from datetime import datetime, timedelta
 import configparser
-import io 
+import io
 
 
 
--- get_ips.sh
+++ get_ips.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 user=$(awk -F "=" '/User/ {print $2}' config.ini)
 FILE=/home/$user/emr_jstat_project/worker_ips.txt
-yarn node -list 2> /dev/null | grep internal | cut -d' ' -f1 | cut -d: -f1 >> '$FILE'
+yarn node -list 2> /dev/null | grep internal | cut -d' ' -f1 | cut -d: -f1 >> "$FILE"
